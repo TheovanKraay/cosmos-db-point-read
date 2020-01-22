7c94313fb47cd68e6350b313a170bf8bc99b7726
TheovanKraay <thvankra@microsoft.com>
2020-01-22 15:48:50 +0000

    add writes

--- CosmosApp/Program.cs
+++ CosmosApp/Program.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.Azure.Cosmos;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace CosmosApp
 {
     class Program
     {
-        private static readonly string containerId = "Item";
-        private static readonly string databaseId = "Movies";
+        private static readonly string containerId = "test";
+        private static readonly string databaseId = "testdb";
 
         private static readonly string endpoint = "endpoint";
         private static readonly string authKey = "authKey";
@@ -16,31 +19,36 @@ namespace CosmosApp
         private static Container container = null;
         static async Task Main(string[] args)
         {
-
-
-
             using (CosmosClient client = new CosmosClient(endpoint, authKey))
-
             {
-
                 await Program.Initialize(client);
-
-                Console.WriteLine("Hello World!");
-                Console.WriteLine("\n1.2 - Reading Item by Id");
+                Console.WriteLine("Getting read and write charges...");
                 // Note that Reads require a partition key to be specified.
                 await Program.ReadItemAsync();
-
             }
-
         }
         private static async Task ReadItemAsync()
         {
             try
             {
                 ItemResponse<T> response = await container.ReadItemAsync<T>(
-                partitionKey: new PartitionKey("Category"), id: "a88b95ec-822c-4a5d-93f2-69465723f353");
+                partitionKey: new PartitionKey("pk1"), id: "b87293f2-fb70-4e20-a913-a270f6d6f9ac");
 
-                Console.WriteLine($"Request charge: {response.RequestCharge}");
+                Console.WriteLine($"Read Request charge: {response.RequestCharge}");
+
+                var json = "[{\"id\": \"" + Guid.NewGuid() + "\",\"pk\": \"pk1\",\"Name\": \"theo\"}," +
+                            "{\"id\": \"" + Guid.NewGuid() + "\",\"pk\": \"pk1\",\"Name\": \"theo\",\"Surname\": \"van Kraay\"}," +
+                            "{\"id\": \"" + Guid.NewGuid() + "\",\"pk\": \"pk1\",\"Name\": \"theo\",\"Surname\": \"van Kraay\",\"Address\": \"Somewhere\"}," +
+                            "{\"id\": \"" + Guid.NewGuid() + "\",\"pk\": \"pk1\",\"Name\": \"theo\",\"Surname\": \"van Kraay\",\"Address\": \"Somewhere\",\"Postcode\": \"FY8353\"}," +
+                            "{\"id\": \"" + Guid.NewGuid() + "\",\"pk\": \"pk1\",\"Name\": \"theo\",\"Surname\": \"van Kraay\",\"Address\": \"Somewhere\",\"Postcode\": \"FY8353\",\"PhoneNumber\": \"98927267728\"}]";
+
+                var docs = JsonConvert.DeserializeObject<List<JObject>>(json);
+
+                foreach (var doc in docs)
+                {
+                    ItemResponse<JObject> response2 =  await container.CreateItemAsync<JObject>(doc, new PartitionKey(doc.GetValue("pk").ToString()));
+                    Console.WriteLine($"Write Request charge: {response2.RequestCharge}");
+                }
 
             }
             catch (Exception e)
@@ -55,7 +63,7 @@ namespace CosmosApp
 
         {
             database = await client.CreateDatabaseIfNotExistsAsync(databaseId);
-            container = await database.CreateContainerIfNotExistsAsync(containerId, "/partitionKey");
+            container = await database.CreateContainerIfNotExistsAsync(containerId, "/pk");
         }
         private class T
         {
